7193169fc722a8da5f5a273b7c6558682e73bd20
imjeongsoo <jeongsuim187@gmail.com>
2022-03-16 09:52:03 +0900

    test

--- Myapp1.java
+++ Myapp1.java
@@ -1,8 +1,12 @@
 public class Myapp1 {
     public static void main(String[] args) {
+        int a;
+        int b;
+        int a + b;
         System.out.println("Hello world !!");
         System.out.println("abc");
         System.out.println("def");
         System.out.println("ahds");
-    }
+        System.out.println(a,b,a+b);
+}
 }
